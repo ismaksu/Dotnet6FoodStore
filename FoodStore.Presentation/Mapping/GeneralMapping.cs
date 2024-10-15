87720a9a999014a7800185d1737fe119df067f42
ismaksu <mcpebagimli@gmail.com>
2024-10-15 12:19:50 +0300

    Added mapping of About and Slider entities.

---
+++ FoodStore.Presentation/Mapping/GeneralMapping.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using FoodStore.Dto.DTOs.AboutDTOs;
+using FoodStore.Dto.DTOs.SliderDTOs;
+using FoodStore.Entity.Concrete;
+
+namespace FoodStore.Presentation.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            //About mapping
+            CreateMap<ResultAboutDto, About>().ReverseMap();
+            CreateMap<CreateAboutDto, About>().ReverseMap();
+            CreateMap<GetByIdAboutDto, About>().ReverseMap();
+            CreateMap<UpdateAboutDto, About>().ReverseMap();
+
+            //Slider mapping
+            CreateMap<ResultSliderDto, Slider>().ReverseMap();
+            CreateMap<CreateSliderDto, Slider>().ReverseMap();
+            CreateMap<GetByIdSliderDto, Slider>().ReverseMap();
+            CreateMap<UpdateSliderDto, Slider>().ReverseMap();
+        }
+    }
+}
